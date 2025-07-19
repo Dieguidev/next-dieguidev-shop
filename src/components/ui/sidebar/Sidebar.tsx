2ebe036b8fa617bcb1168eb61d8cc2fb260a6e63
Dieguidev <diegogaraycullas@gmail.com>
2025-07-19 10:16:15 -0500

    Integrate Zustand UI store for sidebar state; enable open/close menu from TopMenu and Sidebar with blur and transitions

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,21 +1,47 @@
+'use client'
+
 import Link from "next/link"
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
+import { useUIStore } from "@/store"
+import clsx from "clsx"
 
 
 export const Sidebar = () => {
+
+  const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen);
+  const closeMenu = useUIStore(state => state.closeSideMenu);
+
+
   return (
     <div>
       {/* Background black */}
-      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30">
-      </div>
+      {
+        isSideMenuOpen && (
+          <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30">
+          </div>
+        )
+      }
+
 
       {/* Blur */}
-      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm">
-      </div>
+      {
+        isSideMenuOpen && (
+          <div
+            className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+            onClick={closeMenu}
+          />
+
+        )
+      }
+
 
       {/* Sidemenu */}
-      <nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
-        <IoCloseOutline size={50} className="" />
+      <nav className={
+        clsx("fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300", {
+          "translate-x-full": !isSideMenuOpen,
+        })
+      }>
+        <IoCloseOutline size={50} className="absolute top-5 right-5 cursor-pointer" onClick={closeMenu} />
         <div className="relative mt-14">
           <IoSearchOutline size={20} className="absolute top-2 left-2" />
           <input type="text" placeholder="Buscar" className="w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500" />
