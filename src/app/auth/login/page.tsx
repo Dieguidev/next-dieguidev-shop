1fe9efee37a295ffb5abec380cb4445268533d8d
Dieguidev <diegogaraycullas@gmail.com>
2025-07-18 22:53:45 -0500

    Add Shop and Auth layouts and pages; update favicon and move homepage to shop route

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Login() {
+  return (
+    <main className="">
+      <h1>login Page</h1>
+
+
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function NewAccount() {
+  return (
+    <main className="">
+      <h1>new account</h1>
+
+
+    </main>
+  );
+}
