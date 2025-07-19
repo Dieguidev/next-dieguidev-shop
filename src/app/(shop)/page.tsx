e6b6e7b62fd9c4e24aa25bd840099a4eb1b2e866
Dieguidev <diegogaraycullas@gmail.com>
2025-07-18 23:06:03 -0500

    Add initial route structure for shop: admin, cart, category, checkout, empty, orders, product, and products pages; update layout metadata and minor homepage markup adjustment

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,10 +3,10 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1>hola mundo</h1>
       <h1 className={titleFont.className}>hola mundo</h1>
 
-    </main>
+    </div>
   );
 }
