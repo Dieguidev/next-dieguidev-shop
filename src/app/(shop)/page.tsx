80e7e39b86135ab4cead59920ee6aa0d2b8ab6e3
Dieguidev <diegogaraycullas@gmail.com>
2025-07-19 08:52:40 -0500

    Add ProductsGrid component and product interfaces; display products on homepage using seed data

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,7 +1,7 @@
-import { Title } from "@/components";
+import { ProductsGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 
-
-
+const products = initialData.products;
 
 export default function Home() {
   return (
@@ -11,6 +11,8 @@ export default function Home() {
         subtitle="La mejor tienda de productos"
         className="mb-2"
       />
+
+      <ProductsGrid products={products} />
 
     </>
   );
