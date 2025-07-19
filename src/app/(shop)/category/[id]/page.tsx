a81adf734c873cfa4825fb11d8a826902f16b882
Dieguidev <diegogaraycullas@gmail.com>
2025-07-19 13:29:15 -0500

    Refactor product interfaces to use Category, Size, and Type aliases; update category page and menu links for consistency

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,12 +1,35 @@
-interface CategoryPageProps {
-  params: { id: string };
-}
+import { ProductsGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 
-export default function CategoryPage({ params }: CategoryPageProps) {
-  const { id } = params;
+interface CategoryPageProps {
+  params: { id: Category };
+}
+const products = initialData.products;
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const { id } = await params;
+
+  const categoryProducts = products.filter(
+    (product) => product.gender === id
+  );
+
+  const labels: Record<Category, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex': 'para todos',
+  }
+
   return (
-    <div>
-      <h1>Category Page: {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Articulos ${labels[id]}`}
+        subtitle="La mejor tienda de productos"
+        className="mb-2"
+      />
+
+      <ProductsGrid products={categoryProducts} />
+    </>
   );
 }
