8f5074ed7cbbcfc37099bbc0ae8db6d1f317d22f
Dieguidev <diegogaraycullas@gmail.com>
2025-07-20 12:19:36 -0500

    Refactor seed: use flatMap for product images and improve data processing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,12 +11,59 @@ export async function main() {
     await prisma.category.deleteMany(),
   ]);
 
+  // add categories
   const categoriesData = categories.map((categorie) => ({
     name: categorie,
   }));
 
   await prisma.category.createMany({
     data: categoriesData,
+  });
+  // add products
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = new Map(
+    categoriesDB.map((category) => [category.name, category.id])
+  );
+  const productsData = products.map((product) => {
+    const categoryId = categoriesMap.get(product.type);
+    if (!categoryId)
+      throw new Error(`Category not found for type: ${product.type}`);
+    return {
+      title: product.title,
+      description: product.description,
+      inStock: product.inStock,
+      price: product.price,
+      sizes: product.sizes,
+      slug: product.slug,
+      tags: product.tags,
+      gender: product.gender,
+      categoryId: categoryId,
+    };
+  });
+  await prisma.product.createMany({
+    data: productsData,
+  });
+
+  // add productimages
+
+  const productsDB = await prisma.product.findMany();
+  const productsMap = new Map(
+    productsDB.map((product) => [product.slug, product.id])
+  );
+
+  const productImagesData = products.flatMap((product) => {
+    const productId = productsMap.get(product.slug);
+    if (!productId)
+      throw new Error(`Product not found for slug: ${product.slug}`);
+    return product.images.map((image) => ({
+      url: image,
+      productId,
+    }));
+  });
+
+  await prisma.productImage.createMany({
+    data: productImagesData,
   });
 
   console.log("Seeding database...");
