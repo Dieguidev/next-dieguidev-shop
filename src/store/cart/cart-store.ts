a08508602cca12bab054279bf42df734438300f7
Dieguidev <diegogaraycullas@gmail.com>
2025-07-26 08:43:41 -0500

    feat(cart-store): persistencia de carrito con zustand, lógica para agregar y actualizar cantidad de productos

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,32 +1,40 @@
 import { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface State {
   cart: CartProduct[];
   addProductToCart: (product: CartProduct) => void;
 }
 
-export const useCartStore = create<State>()((set, get) => ({
-  cart: [],
-  addProductToCart: (product: CartProduct) => {
-    const { cart } = get();
-    //.1 Check if the product already exists in the cart
-    const existingProductIndex = cart.some(
-      (item) => item.id === product.id && item.size === product.size
-    );
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
+        //.1 Check if the product already exists in the cart
+        const existingProductIndex = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
 
-    if (!existingProductIndex) {
-      set({ cart: [...cart, product] });
-      return;
+        if (!existingProductIndex) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+
+        // If it exists, update the quantity
+        const updatedCart = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+          return item;
+        });
+        set({ cart: updatedCart });
+      },
+    }),
+    {
+      name: "shopping-cart",
     }
-
-    // If it exists, update the quantity
-    const updatedCart = cart.map((item) => {
-      if (item.id === product.id && item.size === product.size) {
-        return { ...item, quantity: item.quantity + product.quantity };
-      }
-      return item;
-    });
-    set({ cart: updatedCart });
-  },
-}));
+  )
+);
