a05cf51e3c8f5595a64bfecc63c1947a51094c50
Dieguidev <diegogaraycullas@gmail.com>
2025-07-26 09:48:33 -0500

    feat(cart): listado visual de productos en el carrito con QuantitySelector y opción de remover

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,20 +1,13 @@
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]
+import { Title } from "@/components";
+import { ProductsInCart } from "./ui/ProductsInCart";
+
+
 
 export default function CartPage() {
 
-  if (productsInCart.length === 0) {
-    redirect('/empty')
-  }
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
@@ -31,31 +24,8 @@ export default function CartPage() {
 
 
             {/* Items */}
-            {
-              productsInCart.map(product => (
-                <div key={product.slug} className="flex mb-5">
-                  <Image
-                    src={`/products/${product.images[0]}`}
-                    width={100}
-                    height={100}
-                    style={{ width: '100px', height: '100px' }}
-                    alt={product.title}
-                    className="mr-5 rounded "
-                    priority
-                  />
+            <ProductsInCart />
 
-                  <div>
-                    <p>{product.title}</p>
-                    <p>{product.price}</p>
-                    <QuantitySelector quantity={3} />
-
-                    <button className="underline mt-3">
-                      Remover
-                    </button>
-                  </div>
-                </div>
-              ))
-            }
           </div>
 
           {/* Checkout */}
