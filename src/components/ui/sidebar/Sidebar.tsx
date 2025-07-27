894ec3158c3765568f87a8e59f16680559c5d41c
Dieguidev <diegogaraycullas@gmail.com>
2025-07-26 23:03:14 -0500

    feat(sidebar): integración de acciones login/logout, mejoras visuales y navegación en Sidebar

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 import { useUIStore } from "@/store"
 import clsx from "clsx"
+import { logout } from "@/actions"
 
 
 export const Sidebar = () => {
@@ -66,10 +67,13 @@ export const Sidebar = () => {
           <span className="ml-3 text-xl">Ingresar</span>
         </Link>
 
-        <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+        <button
+          onClick={logout}
+          className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
           <IoLogOutOutline size={30} />
           <span className="ml-3 text-xl">Salir</span>
-        </Link>
+        </button>
 
         <div className="w-full h-px bg-gray-200 my-10" />
 
