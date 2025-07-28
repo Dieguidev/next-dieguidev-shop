d72acdd5dec2543588c8ffe393f34eeb2f8a4c3b
Dieguidev <diegogaraycullas@gmail.com>
2025-07-28 09:30:31 -0500

    Feat: Implement user registration form\n\n- Added RegisterForm component with form validation using react-hook-form\n- Integrated registerUser action to handle user registration\n- Display error messages for registration failures\n- Updated actions index to export register action

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 import clsx from 'clsx';
+import { registerUser } from "@/actions";
+import { useState } from "react";
 
 type FormInputs = {
   name: string;
@@ -12,16 +14,26 @@ type FormInputs = {
 
 export const RegisterForm = () => {
 
+  const [errorMessage, setErrorMessage] = useState('')
+
   const {
     register,
     handleSubmit,
-    watch,
+    // watch,
     formState: { errors },
   } = useForm<FormInputs>()
 
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    setErrorMessage('')
     const { name, email, password } = data;
-    console.log({ name, email, password });
+    const resp = await registerUser(name, email, password);
+
+    if (!resp.ok) {
+      setErrorMessage(resp.message);
+      return
+    }
+
+    console.log(resp);
 
   }
 
@@ -79,6 +91,8 @@ export const RegisterForm = () => {
         type="password"
         {...register("password", { required: true, minLength: 6 })}
       />
+
+      <span className="text-red-500">{errorMessage}</span>
 
       <button
 
